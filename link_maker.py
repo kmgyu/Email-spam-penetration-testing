be39a6ebe0b4aeabcf4450abea9b225e9841af1f
kmgyu <lidersy961@gmail.com>
2024-12-03 23:33:31 +0900

    bitly 단축 url 테스트

---
+++ link_maker.py
@@ -0,0 +1,39 @@
+import requests
+
+
+TOKEN = 'b91dc18b7348be2e3d0e1e31d7f794019437e1eb'
+url = 'https://api-ssl.bitly.com/v4/shorten'
+
+headers = {
+    'Authorization' : f'Bearer {TOKEN}',
+    'Content-Type' : 'application/json',
+    
+}
+
+body1 ={
+  "long_url": "https://dev.bitly.com",
+  "domain": "bit.ly",
+  "group_guid": "Ba1bc23dE4F",
+  "title": "Bitly API Documentation",
+  "tags": [
+    "bitly",
+    "api"
+  ],
+  "deeplinks": [
+    {
+      "app_id": "com.bitly.app",
+      "app_uri_path": "/store?id=123456",
+      "install_url": "https://play.google.com/store/apps/details?id=com.bitly.app&hl=en_US",
+      "install_type": "promote_install"
+    }
+  ]
+}
+
+body2 = {
+  "long_url": "https://www.youtube.com/watch?v=1Z3Q29stte0",
+  "domain": "bit.ly",
+#   "group_guid": "mokpo_univ_hacking_test"
+}
+
+response = requests.post(url=url, json=body2, headers=headers)
+print(response.json())
